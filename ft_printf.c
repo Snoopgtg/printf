f63ea1251b698fe2765f7879d042f58b6fb96da2
Snoopgtg <Snoopgtg@yahoo.com>
2017-03-07 09:15:41 +0200

    07.03.17

--- ft_printf.c
+++ ft_printf.c
@@ -1,6 +1,30 @@
 #include "ft_printf.h"
 #include <stdio.h>
 
+void	fill_digit(char *s, t_prntf **base)
+{
+	int 	i;
+	char	*t;
+	i = 0;
+	t = s;
+	while (*t)
+	{
+		(*t == '0') ? (*base)->ziro = '0' : 0;
+		if (isdigit_withoutziro(*t))
+		{
+			(*base)->digit = ft_atoi(t) - 1;
+			if ((i + (int)ft_lenitoa((*base)->digit) < (*base)->str_len))
+			{
+				i += (int)ft_lenitoa((*base)->digit);
+				t += ft_lenitoa((*base)->digit);
+			}
+		}
+		t++;
+		i++;
+	}
+
+}
+
 void	fill_basetoziro(t_prntf *base)
 {
 	base->digit = 0;
@@ -13,14 +37,15 @@ void	fill_basetoziro(t_prntf *base)
 
 void 	fill_flags(char *s, t_prntf **base)
 {
+	(*base)->str_len = ft_strlen(s);
 	while (*s && (chack_afte_pers(*s) || ft_isdigit(*s)))
 	{
 		(*s == ' ') ? (*base)->space = ' ' : 0;
 		(*s == '+') ? (*base)->plus = '+' : 0;
 		(*s == '-') ? (*base)->minus = '-' : 0;
-		(*s == '0') ? (*base)->ziro = '0' : 0;
+		//(*s == '0') ? (*base)->ziro = '0' : 0;
 		(*s == '#') ? (*base)->hash = '#' : 0;
-		(isdigit_withoutziro(*s)) ? (*base)->digit = ft_atoi(s) - 1 : 0;
+		//(isdigit_withoutziro(*s)) ? (*base)->digit = ft_atoi(s) - 1 : 0;
 		s++;
 	}
 }
@@ -35,72 +60,90 @@ int 	isdigit_withoutziro(char c)
 int		chack_afte_pers(char c)
 {
 	if (c == 'h' || c == 'l' || c == 'j' || c == 'z' || c == ' ' || c == '.'
-			|| c == '+' || c == '-' || c == '#' /*|| (c >= '0' && c <= '9')*/)
+			|| c == '+' || c == '-' || c == '#')
 		return (1);
 	return (0);
 }
 
-void	print_ziroorspase(char **format, t_prntf *base)
+void	print_ziroorspase(char **format, t_prntf *base, int ***r)
 {
 	int f;
 
 	f = 0;
-	if ((chack_afte_pers(**format) || (ft_isdigit(**format) && **format)))
+	if ((chack_afte_pers(**format) || ft_isdigit(**format)) && **format)
 	{
 		if (base->minus == '-')
 		{
-			while ((chack_afte_pers(**format) || (ft_isdigit(**format) && **format)))
+			while ((chack_afte_pers(**format) || ft_isdigit(**format)) && **format)
 				(*format)++;
 			(**format) ? write(1, &(**format), 1) : 0 ;
 			(**format) ? (*format)++ : 0 ;
-			f = 1;
+			f = 0;
+			(***r)++;
 		}
+		else
+			(base->ziro == '0') ? f = 1 : 0;
+		(***r) += base->digit;
 		while (base->digit > 0)
 		{
-			(!f) ? write(1, "0", 1) : write(1, " ", 1);
+			(!f) ? write(1, " ", 1) : write(1, "0", 1);
 			(base->digit)--;
 		}
 	}
 }
 
-void	chack_only_txt(char *format, t_prntf *base)
+void	chack_only_txt(char *format, t_prntf *base, int **r)
 {
 	while (*format)
 	{
 		if (*format == '%' && format++)
 		{
 			fill_flags(format, &base);
-			print_ziroorspase(&format, base);
+			fill_digit(format, &base);
+			print_ziroorspase(&format, base, &r);
 			while (chack_afte_pers(*format) || ft_isdigit(*format))
 				format++;
-			write(1, &(*format), 1);
+			if (*format)
+			{
+				write(1, &(*format), 1);
+				(**r)++;
+			}
 		}
 		else
+		{
 			write(1, &(*format), 1);
-		format++;
+			(**r)++;
+		}
+		(*format) ? format++ : 0 ;
 	}
-
 }
 
-void	fill_formater(char *format, t_prntf *base)
+void	fill_formater(char *format, t_prntf *base, int *r)
 {
 
-	chack_only_txt(format, base);
+	chack_only_txt(format, base, &r);
 
 }
 
 
 int		ft_printf(char *format, ...)
 {
+	int r;
+
+	r = 0;
 	t_prntf base[1];
 	fill_basetoziro(base);
-	fill_formater(format, base);
-	return (0);
+	fill_formater(format, base, &r);
+	return (r);
 }
 int main()
 {
-	ft_printf("my ="STR, 1);
-	ft_printf("no ="STR, 1);
+	int k;
+
+	k = ft_printf("%20.5%");
+	ft_printf("\n");
+	printf("%20.5%");
+	printf("k = %d", k);
 	//printf("[%ld]", (long)(-1));
 	return 0;
 }
