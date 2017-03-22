2e31136f5a4a3de0af34fadd1390efbca62b28a6
Snoopgtg <Snoopgtg@yahoo.com>
2017-03-22 12:50:09 +0200

    перед переделкой вивода в строку. зараз все виводить відразу не перероблюючи в строку

--- cmake-build-debug/CMakeFiles/ft_printf.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/ft_printf.dir/cmake_clean.cmake
@@ -3,6 +3,7 @@ file(REMOVE_RECURSE
   "CMakeFiles/ft_printf.dir/fill.c.o"
   "CMakeFiles/ft_printf.dir/main.c.o"
   "CMakeFiles/ft_printf.dir/putnbr.c.o"
+  "CMakeFiles/ft_printf.dir/ft_itoa_base.c.o"
   "ft_printf.pdb"
   "ft_printf"
 )
